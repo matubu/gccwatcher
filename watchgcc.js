5b67352da26a9b8610f3cb7c9a8346f729a2086c
matubu <78111467+matubu@users.noreply.github.com>
2021-12-29 13:54:00 +0100

    Update watchgcc.js

--- watchgcc.js
+++ watchgcc.js
@@ -1,8 +1,9 @@
 const	watcher = Deno.watchFs([Deno.args[0]]);
 let		process = Deno.run({ cmd: ["make", "run", "-C", Deno.args[0]] });
 
-for await (const e of watcher) {
-	if (e.paths.some((file) => /(\.(c|h)(pp))$/.test(file) || file === "Makefile"))
+for await (const e of watcher)
+{
+	if (e.paths.some(file => /(\.(c|h)(pp)?|(\/|^)Makefile)$/.test(file)))
 	{
 		Deno.run({ cmd: ["kill", process.pid] });
 		process = Deno.run({ cmd: ["make", "run", "-C", Deno.args[0]] });
