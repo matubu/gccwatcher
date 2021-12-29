5d3af31238f7385926ea75be55f8c545800bdb38
matubu <78111467+matubu@users.noreply.github.com>
2021-12-29 14:20:22 +0100

    custom regex

--- watchgcc.js
+++ watchgcc.js
@@ -1,11 +1,12 @@
-const	watcher = Deno.watchFs([Deno.args[0]]);
-let		process = Deno.run({ cmd: ["make", "run", "-C", Deno.args[0]] });
+const	watcher = Deno.watchFs([Deno.args[0]])
+let		process = Deno.run({ cmd: ["make", "run", "-C", Deno.args[0]] })
+let		regex = Deno.args[1] ? RegExp(Deno.args[1]) : /(\.(c|h)(pp)?|(\/|^)Makefile)$/
 
 for await (const e of watcher)
 {
-	if (e.paths.some(file => /(\.(c|h)(pp)?|(\/|^)Makefile)$/.test(file)))
+	if (e.paths.some(file => regex.test(file)))
 	{
 		Deno.run({ cmd: ["kill", process.pid] });
-		process = Deno.run({ cmd: ["make", "run", "-C", Deno.args[0]] });
+		process = Deno.run({ cmd: ["make", "run", "-C", Deno.args[0]] })
 	}
 }
